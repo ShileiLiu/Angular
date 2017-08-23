5bb852a38c45dd71641c75c1a84d4dff0c710dd6
Jarvan <liushileijarvan@gmail.com>
2017-08-23 10:00:52 +0800

    p

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppComponents2 } from './app.components2';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppComponents2
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,AppComponents2]
 })
 export class AppModule { }
